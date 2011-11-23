827ebab085863df556fa9ae9c51c0487bd9b59ae
Aram Hăvărneanu <aram@mgk.ro>
2011-11-23 22:12:05 +0200

    dotfiles: fixed install and clean scripts

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ if ! make --version 2>/dev/null | grep 'GNU Make' >/dev/null; then
         MAKE=gmake
 fi
 
-find . -mindepth 1 -maxdepth 1 -type d | grep -v hg | xargs "$MAKE" install -C
+find . -mindepth 1 -maxdepth 1 -type d | grep -v hg | xargs -n1 "$MAKE" install -C
