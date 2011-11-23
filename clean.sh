018af0174d6698bc432d0eb9aef46958a8375fa5
Aram Hăvărneanu <aram@mgk.ro>
2011-11-23 22:04:49 +0200

    dotfiles: added clean.sh

---
+++ clean.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+MAKE=make
+if ! make --version 2>/dev/null | grep 'GNU Make' >/dev/null; then
+        MAKE=gmake
+fi
+
+find . -mindepth 1 -maxdepth 1 -type d | grep -v hg | xargs "$MAKE" clean -C
