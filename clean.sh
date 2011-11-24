b40abbf964c43ab527ad79238a61d69babe17162
Aram Hăvărneanu <aram@mgk.ro>
2011-11-24 15:50:10 +0200

    dotfiles: got rid of complicated build infrastructure

--- clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-MAKE=make
-if ! make --version 2>/dev/null | grep 'GNU Make' >/dev/null; then
-        MAKE=gmake
-fi
-
-find . -mindepth 1 -maxdepth 1 -type d | grep -v hg | xargs -n1 "$MAKE" clean -C
