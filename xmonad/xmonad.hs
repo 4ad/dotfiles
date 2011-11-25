c389e4ec8dae0775b525fb3f085e6b24b84d27fe
Aram Hăvărneanu <aram@mgk.ro>
2011-11-25 03:36:24 +0200

    xmonad: disable GridSelect

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -7,16 +7,19 @@
 import XMonad
 import XMonad.Actions.GridSelect
  
-import qualified XMonad.StackSet as W
-import qualified Data.Map as M
+-- import qualified XMonad.StackSet as W
+-- import qualified Data.Map as M
  
 main = do
   xmonad $ defaultConfig
-    { keys = myKeys 
-    }
+--    { modMask            = myModMask
+--    , keys               = myKeys
+--    }
  
-myGSConfig = defaultGSConfig
-myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
-    [ ((mod1Mask, xK_Tab), goToSelected myGSConfig) -- display grid select and go to selected window
-    , ((mod4Mask, xK_Tab), gridselectWorkspace myGSConfig W.view) -- display grid select and go to selected workspac
-    ]
+-- myModMask = mod4Mask -- Windows key
+-- myGSConfig = defaultGSConfig
+
+-- myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
+--    [ ((mod1Mask, xK_Tab), goToSelected myGSConfig) -- display grid select and go to selected window
+--    , ((mod4Mask, xK_Tab), gridselectWorkspace myGSConfig W.view) -- display grid select and go to selected workspac
+--    ]
