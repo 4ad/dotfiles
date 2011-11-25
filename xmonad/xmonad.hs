36a67f8cc67a730510fa136ff661799aed34f5b1
Aram Hăvărneanu <aram@mgk.ro>
2011-11-25 02:57:34 +0200

    xmonad: added

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,25 @@
+------------------------------------------------------------------------
+-- ~/.xmonad/xmonad.hs
+-- validate syntax: xmonad --recompile
+{-# LANGUAGE NoMonomorphismRestriction #-}
+------------------------------------------------------------------------
+ 
+import XMonad
+import XMonad.Actions.GridSelect
+ 
+import qualified XMonad.StackSet as W
+import qualified Data.Map as M
+ 
+main = do
+  xmonad $ defaultConfig
+    { modMask            = myModMask
+    , keys               = myKeys
+    }
+ 
+myModMask = mod4Mask -- Windows key
+myGSConfig = defaultGSConfig
+
+myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
+    [ ((mod1Mask, xK_Tab), goToSelected myGSConfig) -- display grid select and go to selected window
+    , ((mod4Mask, xK_Tab), gridselectWorkspace myGSConfig W.view) -- display grid select and go to selected workspac
+    ]
