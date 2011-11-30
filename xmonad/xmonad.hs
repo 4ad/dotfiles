7cef290523e90bfc974a227d9f14e46226763ac0
Aram Hăvărneanu <aram@mgk.ro>
2011-11-30 18:21:20 +0200

    xmonad: remov MouseResizableTile until the border bug is fixed

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -6,26 +6,14 @@
  
 import XMonad
 import XMonad.Hooks.EwmhDesktops
-import XMonad.Layout.MouseResizableTile
 import XMonad.Layout.NoBorders
 import XMonad.Util.EZConfig(additionalKeys)
  
 main =
     xmonad $ defaultConfig
         { handleEventHook    = fullscreenEventHook
-        , layoutHook = myLayout
         } `additionalKeys`
         ( myScrotKeys )
-
-myLayout = smartBorders tiled ||| smartBorders mirrorTiled ||| noBorders Full
-  where
-    tiled = mouseResizableTile
-        { draggerType = BordersDragger 
-        }
-    mirrorTiled = mouseResizableTile
-        { isMirrored = True
-        , draggerType = BordersDragger
-        }
 
 myScrotKeys = 
     [ ((mod1Mask, xK_Print), spawn "sleep 0.2; scrot -s") -- take a screen shot of a window
