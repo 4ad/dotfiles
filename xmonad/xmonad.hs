0e5886e35a76ab69a13459a30ee04ddd8f092725
Aram Hăvărneanu <aram@mgk.ro>
2011-11-30 14:58:48 +0200

    xmonad: modularized custom keys

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -18,11 +18,16 @@ main =
     xmonad $ defaultConfig
         { handleEventHook    = fullscreenEventHook
         } `additionalKeys`
-        [ ((mod1Mask, xK_Tab), goToSelected myGSConfig) -- display grid select and go to selected window
-        , ((mod4Mask, xK_Tab), gridselectWorkspace myGSConfig W.view) -- display grid select and go to selected workspace
-        , ((mod1Mask, xK_Print), spawn "sleep 0.2; scrot -s") -- take a screen shot of the window
-        , ((0, xK_Print), spawn "scrot") -- take a screen shot of the screen
-        ]
+        ( myGSKeys ++ myScrotKeys )
  
 myGSConfig = defaultGSConfig
 
+myGSKeys = 
+    [ ((mod1Mask, xK_Tab), goToSelected myGSConfig) -- display grid select and go to selected window
+    , ((mod4Mask, xK_Tab), gridselectWorkspace myGSConfig W.view) -- display grid select and go to selected workspace
+    ]
+
+myScrotKeys = 
+    [ ((mod1Mask, xK_Print), spawn "sleep 0.2; scrot -s") -- take a screen shot of the window
+    , ((0, xK_Print), spawn "scrot") -- take a screen shot of the screen
+    ]
