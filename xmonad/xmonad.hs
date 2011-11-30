581f34b6650a27273aa4fb1039ccb9ca5d0d17d6
Aram Hăvărneanu <aram@mgk.ro>
2011-11-30 18:19:47 +0200

    xmonad: added MouseResizableTile

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -5,21 +5,28 @@
 ------------------------------------------------------------------------
  
 import XMonad
-import XMonad.Actions.GridSelect
-import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Layout.MouseResizableTile
+import XMonad.Layout.NoBorders
 import XMonad.Util.EZConfig(additionalKeys)
-import System.IO
- 
-import qualified XMonad.StackSet as W
-import qualified Data.Map as M
  
 main =
     xmonad $ defaultConfig
         { handleEventHook    = fullscreenEventHook
+        , layoutHook = myLayout
         } `additionalKeys`
         ( myScrotKeys )
- 
+
+myLayout = smartBorders tiled ||| smartBorders mirrorTiled ||| noBorders Full
+  where
+    tiled = mouseResizableTile
+        { draggerType = BordersDragger 
+        }
+    mirrorTiled = mouseResizableTile
+        { isMirrored = True
+        , draggerType = BordersDragger
+        }
+
 myScrotKeys = 
     [ ((mod1Mask, xK_Print), spawn "sleep 0.2; scrot -s") -- take a screen shot of a window
     , ((0, xK_Print), spawn "scrot") -- take a screen shot of the screen
